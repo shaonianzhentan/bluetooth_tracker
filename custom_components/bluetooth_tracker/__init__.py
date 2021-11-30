be4410bf525b58f7fbd271d18d9b1583a4f81933
zhulu <zhululu@weihaizixun.com>
2021-11-30 18:03:53 +0800

    初始化项目

---
+++ custom_components/bluetooth_tracker/__init__.py
@@ -0,0 +1,12 @@
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+import homeassistant.helpers.config_validation as cv
+from .const import DOMAIN
+
+CONFIG_SCHEMA = cv.deprecated(DOMAIN)
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return True
---
+++ custom_components/bluetooth_tracker/const.py
@@ -0,0 +1,1 @@
+DOMAIN = "bluetooth_tracker"
