ce66def23b4bc957c2c590444bb79fcccc006415
zhulu <zhululu@weihaizixun.com>
2021-12-01 15:50:12 +0800

    update

--- custom_components/bluetooth_tracker/__init__.py
+++ custom_components/bluetooth_tracker/__init__.py
@@ -6,7 +6,9 @@ from .const import DOMAIN
 CONFIG_SCHEMA = cv.deprecated(DOMAIN)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    print('async_setup_entry', entry.data)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    print('async_unload_entry', entry.data)
     return True
--- custom_components/bluetooth_tracker/config_flow.py
+++ custom_components/bluetooth_tracker/config_flow.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 from typing import Any
 import voluptuous as vol
+from homeassistant.helpers.device_registry import format_mac
+import homeassistant.helpers.config_validation as cv
 
-from homeassistant.config_entries import ConfigFlow
-from homeassistant.data_entry_flow import FlowResult
+from homeassistant.core import callback
+from homeassistant.config_entries import ConfigFlow, OptionsFlow, ConfigEntry
 
 from .const import DOMAIN
 
@@ -17,12 +19,47 @@ class SimpleConfigFlow(ConfigFlow, domain=DOMAIN):
 
     VERSION = 1
 
-    async def async_step_user(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
+    async def async_step_user(self, user_input = None):
     
         if user_input is None:
             errors = {}
+            entity_list = {}
+            # 读取当前人员
+            states = self.hass.states.async_all()
+            for state in states:
+                domain = state.domain
+                if domain == 'person':
+                    attributes = state.attributes
+                    entity_id = state.entity_id
+                    friendly_name = attributes.get('friendly_name')
+                    if friendly_name is not None:
+                        entity_list[entity_id] = friendly_name
+
+            DATA_SCHEMA = vol.Schema({
+                vol.Required("ip"): str,
+                vol.Required("mac"): str,                
+                vol.Required("person", default=[]): vol.In(entity_list)
+            })
             return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors=errors)
 
-        return self.async_create_entry(title=DOMAIN, data=user_input)
+        return self.async_create_entry(title=user_input['ip'], data=user_input)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(entry: ConfigEntry):
+        return OptionsFlowHandler(entry)
+
+class OptionsFlowHandler(OptionsFlow):
+    def __init__(self, config_entry: ConfigEntry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        return await self.async_step_user(user_input)
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+        if user_input is None:
+            errors = {}
+            return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors=errors)
+        # 选项更新
+        return self.async_create_entry(title=user_input['ip'], data={})
